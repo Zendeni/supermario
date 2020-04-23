dd5e2fceefb546dacd0eeab8bf798da8ae09e84d
Zendeni <buskes.e@gmail.com>
2020-04-23 17:33:01 +0200

    first commit

---
+++ public/js/Level.js
@@ -0,0 +1,38 @@
+import Compositor from './Compositor.js';
+import EventEmitter from './EventEmitter.js';
+import MusicController from './MusicController.js';
+import EntityCollider from './EntityCollider.js';
+import TileCollider from './TileCollider.js';
+
+export default class Level {
+    constructor() {
+        this.gravity = 1500;
+        this.totalTime = 0;
+
+        this.events = new EventEmitter();
+
+        this.music = new MusicController();
+
+        this.comp = new Compositor();
+        this.entities = new Set();
+
+        this.entityCollider = new EntityCollider(this.entities);
+        this.tileCollider = new TileCollider();
+    }
+
+    update(gameContext) {
+        this.entities.forEach(entity => {
+            entity.update(gameContext, this);
+        });
+
+        this.entities.forEach(entity => {
+            this.entityCollider.check(entity);
+        });
+
+        this.entities.forEach(entity => {
+            entity.finalize();
+        });
+
+        this.totalTime += gameContext.deltaTime;
+    }
+}
